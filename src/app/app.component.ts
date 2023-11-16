0544d08247d41e4e22321aabc1b660bed1a4dbcd
Jhon David Pita Macias <jhondavidpitapita@hotmail.com>
2023-11-16 10:43:53 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Weather } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'weatherapp';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  weather: Weather | null = null;
+
+  ngOnInit(): void {
+    this.http
+      .get<Weather>(
+        'http://api.weatherapi.com/v1/current.json?key=1e58e9ed016a497c9e1145804231611&q=Portoviejo&aqi=no'
+      )
+      .subscribe(
+        (response) => {
+          this.weather = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
